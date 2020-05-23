9967d2e20455d956a412252e590feb42ee140ac2
Stefano Ariotta <32983967+st-ario@users.noreply.github.com>
2020-05-23 20:31:05 +0200

    bug fixed

--- src/MCTS.hs
+++ src/MCTS.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module MCTS where
 
@@ -45,7 +46,7 @@ simulation gen state = undefined
 
 type ValueOfNode = Integer
 type NumberOfVisits = Integer
-type Weight s = Maybe (ST s (STRef s (ValueOfNode,NumberOfVisits)))
+type Weight s = Maybe (STRef s (ValueOfNode,NumberOfVisits))
 
 -- nodes for the baby case
 data MCNode' s = MCN { _lastPlayer :: Token
@@ -82,25 +83,20 @@ seedGrid :: MCNode' s -> (MCNode' s,[MCNode' s])
 seedGrid node = (node, legalNodes' node)
 
 -- game tree starting from a given (lastPlayer, board) state
-unfoldGame :: (Token, Grid Token) -> MCTree s
-unfoldGame (lastPlayer, g) = unfoldTree seedGrid $ MCN lastPlayer g Nothing
-
--- for testing purposes:
--- game tree of a single tic-tac-toe match
-babyMCTree :: MCTree s
-babyMCTree = unfoldGame (O, gI)
-  where gI = toGrid $ take 9 $ cycle [EM]
-
-babyMCTList = T.levels babyMCTree
+unfoldGame :: (Token, Grid Token) -> ST s (MCTree s)
+unfoldGame (lastPlayer, g) = return $ unfoldTree seedGrid $ MCN lastPlayer g Nothing
 
 -- ###################  Monte Carlo Tree Search Algorithm ######################
--- NB: ZipNode MCNode' s = (MCTree s, [Position (MCNode' s)])
+-- NB: ZipNode (MCNode' s) = (MCTree s, [Position (MCNode' s)])
 
 -- sets the weight of a node to (0,0)
-setToZero :: MCTree s -> MCTree s
-setToZero (T.Node r sf) = (T.Node r' sf)
-  where r' = set weight (Just $! newSTRef (0,0)) r
--- test whether the bang is useful
+setToZero :: ST s (MCTree s) -> ST s (MCTree s)
+setToZero t = do
+  (T.Node r sf) <- t
+  x <- newSTRef (0,0)
+  let newRoot = set weight (Just $! x) r
+  -- test whether the bang is useful
+  return (T.Node newRoot sf)
 
 {--
 -- sets all the children's weight to (0,0)
@@ -120,10 +116,11 @@ ucb value nChild nParent  = (w / nc) + const * (sqrt $ 2 * (log np) / nc)
 backprop :: ZipNode (MCNode' s) -> Integer -> ST s ()
 backprop (t,ps) diff = do
   let incr (v,n) =  (v + diff , n + 1)
-  x <- fromJust . _weight . T.rootLabel $ t
+  let x = fromJust . _weight . T.rootLabel $ t
+  -- x :: STRef (Integer, Integer)
   modifySTRef' x incr
   y <- readSTRef x
-  trace ("The current weight is " ++ show y) $ pure ()
+  -- trace ("The current weight is " ++ show y) $ pure ()
   if length ps == 0
     then return ()
     else backprop (stepBack (t,ps)) diff
@@ -131,12 +128,12 @@ backprop (t,ps) diff = do
 -- returns the index of the first child maximizing ucb
 bestChild :: MCTree s -> ST s Int
 bestChild t = do
-  parentWeight <- (fromJust . _weight . T.rootLabel $ t) >>= readSTRef
-  -- parentWeight :: (Integer, Integer)
+  parentWeight <- readSTRef $ (fromJust . _weight . T.rootLabel $ t)
+  -- parentWeight :: (ValueOfNode,NumberOfVisits) = (Integer, Integer)
   let parentN = snd parentWeight
-  let wrappedValues = [x | x <- fmap (fromJust . _weight . T.rootLabel) (T.subForest t)]
-  -- wrappedValues :: [ST s (STRef s (Integer, Integer))]
-  valuesList <- forM wrappedValues (\x -> x >>= readSTRef)
+  let refValues = [x | x <- fmap (fromJust . _weight . T.rootLabel) (T.subForest t)]
+  -- refValues :: [STRef s (Integer,Integer)]
+  valuesList <- forM refValues readSTRef
   -- valuesList :: [(Integer, Integer)]
   -- trace (show valuesList) $ pure ()
   let toMaximize (v,c) = ucb v c parentN
@@ -147,56 +144,54 @@ bestChild t = do
 
 expand :: R.StdGen -> Token -> ZipNode (MCNode' s) -> ST s (MCTree s)
 expand gen winner (t,ps) = do
-  let newTree = setToZero t
+  newTree <- setToZero $ return t
   let state = T.rootLabel newTree
   let result = simulationTTT gen (_lastPlayer state, _currentGrid state)
   -- trace (show result) $ pure ()
   let diff = gridOutcome winner (snd result)
-  trace (show diff) $ pure ()
+  -- trace (show diff) $ pure ()
   backprop (newTree, ps) diff
   return newTree
 
-mctsAlgorithm :: R.StdGen -> Integer -> Token -> ZipNode (MCNode' s) -> ST s (Token, Grid Token)
-mctsAlgorithm gen iteration winner (t, ps)
+mctsAlgorithm :: R.StdGen -> Integer -> Token -> ST s (ZipNode (MCNode' s)) -> ST s (Token, Grid Token)
+mctsAlgorithm gen iteration winner wZipper --(t, ps)
   -- after computationalBudget iterations, return the best children of root
   -- according to ucb
-  | iteration >= computationalBudget =
-    if length (T.subForest t) == 0
-      then error "Invalid call to mctsAlgorithm"
-      else do
-        n <- bestChild t
-        let child = T.rootLabel (T.subForest t !! n)
-        return (_lastPlayer child, _currentGrid child)
-  -- when reaching a leaf of the game tree, backpropagate and restart from root
-  -- | length (T.subForest t) == 0 = trace (show $ _currentGrid . T.rootLabel $ t) $ do
-  | length (T.subForest t) == 0 = do
-      let diff = gridOutcome winner (_currentGrid . T.rootLabel $ t)
-      backprop (t,ps) diff
-      mctsAlgorithm gen (iteration + 1) winner (allTheWayBack (t,ps))
-  -- when reaching a node whose children's weights are not initialized,
-  -- initialize all children, run simulations and backpropagate once for
-  -- each of them, then restart from root
-  -- | isNothing $ _weight $ T.rootLabel (T.subForest t !! 0) = trace (show $ _currentGrid . T.rootLabel $ t) $ do
-  | isNothing $ _weight $ T.rootLabel (T.subForest t !! 0) = do
-      let n = length (T.subForest t) -1
-      let f x = expand gen winner (descendTo (t,ps) x)
-      newSubForest <- forM [0..n] f
-      let newTree = T.Node {T.rootLabel=(T.rootLabel t) , T.subForest=newSubForest}
-      -- begin debug stuff
-      let ls = fmap (fromJust . _weight) (newSubForest !! 0)
-      a <- T.rootLabel ls
-      b <- readSTRef a
-      trace ("The weight of the head of the subforest is " ++ show b) $ pure ()
-      --end debug stuff
-      mctsAlgorithm gen (iteration + 1) winner (allTheWayBack (newTree,ps))
-  -- when reaching a node whose children's weights have already been
-  -- initialized, pick the best children according to ucb and restart from it
-  -- | otherwise = trace (show $ _currentGrid . T.rootLabel $ t) $ do
-  | otherwise = do
+  | iteration >= computationalBudget = do
+      (t,ps) <- wZipper
       n <- bestChild t
-      mctsAlgorithm gen iteration winner (descendTo (t, ps) n)
-      -- NB the iteration number is not incremented, as the algorithm
-      -- is not getting back to the root
+      let child = T.rootLabel (T.subForest t !! n)
+      return (_lastPlayer child, _currentGrid child)
+  | otherwise = do
+      (t,ps) <- wZipper
+      if
+        -- when reaching a leaf of the game tree, backpropagate and restart from root
+        | length (T.subForest t) == 0 -> do
+            -- trace (show $ _currentGrid . T.rootLabel $ t) $ return ()
+            let diff = gridOutcome winner (_currentGrid . T.rootLabel $ t)
+            backprop (t,ps) diff
+            let newZipper = return $ allTheWayBack (t,ps)
+            mctsAlgorithm gen (iteration + 1) winner newZipper
+        -- when reaching a node whose children's weights are not initialized,
+        -- initialize all children, run simulations and backpropagate once for
+        -- each of them, then restart from root
+        | isNothing $ _weight $ T.rootLabel (T.subForest t !! 0) -> do
+            -- trace (show $ _currentGrid . T.rootLabel $ t) $ return ()
+            let n = length (T.subForest t) -1
+            let f x = expand gen winner (descendTo (t,ps) x)
+            newSubForest <- forM [0..n] f
+            let newTree = T.Node {T.rootLabel=(T.rootLabel t) , T.subForest=newSubForest}
+            let newZipper = return $ (allTheWayBack (newTree,ps))
+            mctsAlgorithm gen (iteration + 1) winner newZipper
+        -- when reaching a node whose children's weights have already been
+        -- initialized, pick the best children according to ucb and restart from it
+        | otherwise -> do
+            -- trace (show $ _currentGrid . T.rootLabel $ t) $ return ()
+            n <- bestChild t
+            let newZipper = return $ (descendTo (t, ps) n)
+            mctsAlgorithm gen iteration winner newZipper
+            -- NB the iteration number is not incremented, as the algorithm
+            -- is not getting back to the root
     where computationalBudget = 1 :: Integer
 
 getBestMove :: R.StdGen -> (Token, Grid Token) -> (Token, Grid Token)
@@ -204,5 +199,7 @@ getBestMove gen (lastPlayer, g) =
   if isOver g
     then error "No legal moves available"
     else runST $ do
-      let gameTree = (setToZero $ unfoldGame (lastPlayer, g), [])
-      mctsAlgorithm gen 0 (nextPlayer lastPlayer) gameTree
+      gameTree <- setToZero $ unfoldGame (lastPlayer, g)
+      let gameZipper = return $ (gameTree, [])
+      -- gameZipper :: ST s (ZipNode (MCNode' s))
+      mctsAlgorithm gen 0 (nextPlayer lastPlayer) gameZipper
